087d4675bf4e08763afbff7fcf03980ee8c7eceb
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-07-23 17:58:08 -0300

    feat: criando endpoint get user

--- src/main/java/com/devlucas/userdept/controllers/UserController.java
+++ src/main/java/com/devlucas/userdept/controllers/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,12 @@ public class UserController {
 	@GetMapping
 	public List<User> findAll(){
 		List<User> result = repository.findAll();
+		return result;
+	}
+	
+	@GetMapping(value = "/{id}")
+	public User findById(@PathVariable Long id){
+		User result = repository.findById(id).get();
 		return result;
 	}
 	
